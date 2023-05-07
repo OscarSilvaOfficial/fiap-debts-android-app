fe9ec6917994eb969117d4952450ba148ac81b7e
Oscar da Silva <oscarkaka222@gmail.com>
2023-05-07 17:37:45 -0300

    feat: add firestore for retrive debits

--- app/src/main/java/com/fiap/twinttler/Adapters/DebtAdapter.java
+++ app/src/main/java/com/fiap/twinttler/Adapters/DebtAdapter.java
@@ -1,4 +1,4 @@
-package com.fiap.twinttler;
+package com.fiap.twinttler.Adapters;
 
 
 import android.view.LayoutInflater;
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.fiap.twinttler.Entities.Debt;
+import com.fiap.twinttler.R;
 
 import java.util.List;
 import java.util.Locale;
--- app/src/main/java/com/fiap/twinttler/DebtsActivity.java
+++ app/src/main/java/com/fiap/twinttler/DebtsActivity.java
@@ -1,31 +1,59 @@
 package com.fiap.twinttler;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.fiap.twinttler.Adapters.DebtAdapter;
 import com.fiap.twinttler.Entities.Debt;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DebtsActivity extends AppCompatActivity {
 
-    private List<Debt> debts = new ArrayList<>();
+    private FirebaseFirestore db;
+    private final List<Debt> debts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debts);
 
-        debts.add(new Debt("Dívida do cartão de crédito", 500.0));
-        debts.add(new Debt("Empréstimo pessoal", 2000.0));
-        debts.add(new Debt("Dívida do carro", 10000.0));
+        db = FirebaseFirestore.getInstance();
+        db.collection("debits")
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @SuppressLint("NotifyDataSetChanged")
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Map<String, Object> data = document.getData();
+                            String title = (String) data.get("title");
+                            String amount = (String) data.get("value");
+                            debts.add(new Debt(title, Double.parseDouble(amount)));
+                        }
 
-        DebtAdapter adapter = new DebtAdapter(debts);
-
-        RecyclerView recyclerView = findViewById(R.id.debtsRecyclerView);
-        recyclerView.setAdapter(adapter);
+                        RecyclerView recyclerViewDebits = findViewById(R.id.debtsRecyclerView);
+                        DebtAdapter adapter = new DebtAdapter(debts);
+                        recyclerViewDebits.setAdapter(adapter);
+                    } else {
+                        Log.e("DebitsActivity", "Error getting documents: ", task.getException());
+                    }
+                }
+            })
+            .addOnFailureListener(e -> Toast.makeText(DebtsActivity.this, "Erro ao obter a lista de débitos", Toast.LENGTH_SHORT).show());;
     }
 }
